ed5e1057235576d9ec46acc1135affa24fb9c904
iskalinina <is.bykova04@gmail.com>
2023-12-20 17:13:03 +0500

    Fixed markup&styles tours according to self-test

--- source/js/modules/sliders/slider-tours.js
+++ source/js/modules/sliders/slider-tours.js
@@ -16,16 +16,19 @@ const initSliderTours = () => {
         1200: {
           slidesPerView: 3,
           spaceBetween: 30,
+          initialSlide: 1,
         },
 
         768: {
           slidesPerView: 2,
           spaceBetween: 18,
+          initialSlide: 1,
         },
 
         320: {
           slidesPerView: 1,
           spaceBetween: 0,
+          initialSlide: 1,
         },
       },
     });
